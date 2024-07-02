b3792c9407a0bb297c9c9f0cc910e5f423a1d080
zayedansari2 <zayedansari112@gmail.com>
2024-07-01 20:00:10 -0700

    Delete HomeworkTrackerApp.java

--- HomeworkTrackerApp.java
+++
@@ -1,7 +0,0 @@
-public class HomeworkTrackerApp {
-    //Main method
-    public static void main(String[] args) {
-        Assignment myAssignments = new Assignment();
-        myAssignments.createNewTracker();
-    }
-}
